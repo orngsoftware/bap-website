6946a31d4bd758d0ea5d694fb083de02c0aaa6b4
Marat <marat.karlin@gmail.com>
2026-03-09 10:48:48 +0200

    Finish responsive navbar; Hamburger icon animation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,25 @@ import Image from "next/image"
 import Link from "next/link"
 import { useTranslations } from "next-intl"
 import LingoSwitcher from "./switchLingo"
+import { useState } from "react"
 
 const Navbar = () => {
     const t = useTranslations('Navbar')
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
+        <>
+        {isOpen &&
+        <div className="nav-bar-mobile">
+            <Link href="/">{t('1')}</Link>
+            <Link href="/">{t('2')}</Link>
+            <Link href="/">{t('3')}</Link>
+            <Link href="/">{t('4')}</Link>
+            <Link href="/">{t('5')}</Link>
+            <Link href="/">{t('6')}</Link>
+            <Link href="/">{t('7')}</Link> 
+        </div>
+        }
         <div className='nav-wrapper'>
             <Image 
             src='/logo.png' 
@@ -17,16 +31,29 @@ const Navbar = () => {
             height={72}
             />
             <div className='nav-bar'>
-            <Link href="/">{t('1')}</Link>
-            <Link href="/">{t('2')}</Link>
-            <Link href="/">{t('3')}</Link>
-            <Link href="/">{t('4')}</Link>
-            <Link href="/">{t('5')}</Link>
-            <Link href="/">{t('6')}</Link>
-            <Link href="/">{t('7')}</Link>
+                <Link href="/">{t('1')}</Link>
+                <Link href="/">{t('2')}</Link>
+                <Link href="/">{t('3')}</Link>
+                <Link href="/">{t('4')}</Link>
+                <Link href="/">{t('5')}</Link>
+                <Link href="/">{t('6')}</Link>
+                <Link href="/">{t('7')}</Link>
             </div>
-            <LingoSwitcher />
+            <div className="row" style={{gap: 15}}>
+                <LingoSwitcher />
+                <button
+                    onClick={() => setIsOpen(!isOpen)}
+                    className="hamburger clickable"
+                    aria-label={isOpen ? "Close menu" : "Open menu"}
+                    aria-expanded={isOpen}
+                >
+                    <span className={`hamburger-bar top${isOpen ? " open" : ""}`} />
+                    <span className={`hamburger-bar middle${isOpen ? " open" : ""}`} />
+                    <span className={`hamburger-bar bottom${isOpen ? " open" : ""}`} />
+                </button>
+            </div>
         </div>
+        </>
     )
 }
 
