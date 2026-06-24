97329b209f821e347c208101564aef3d8e998085
Marat <marat.karlin@gmail.com>
2026-06-24 17:53:56 +0300

    Update email

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -30,10 +30,10 @@ export default function Contact() {
                 <div className="diff-color-bg" style={{padding: 25, gap: 20}}>
                    <h3 style={{alignSelf: "flex-start"}}>{t('heading1')}</h3>
                    <div className="col" style={{gap: 15}}>
-                        <Link href="mailto:somefuture@email.com" style={{fontWeight: "normal"}}>
+                        <Link href="mailto:info@bap-org.com" style={{fontWeight: "normal"}}>
                             <div className="icon-row">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="var(--almost-black-color)" viewBox="0 0 256 256"><path d="M224,48H32a8,8,0,0,0-8,8V192a16,16,0,0,0,16,16H216a16,16,0,0,0,16-16V56A8,8,0,0,0,224,48Zm-96,85.15L52.57,64H203.43ZM98.71,128,40,181.81V74.19Zm11.84,10.85,12,11.05a8,8,0,0,0,10.82,0l12-11.05,58,53.15H52.57ZM157.29,128,216,74.18V181.82Z"></path></svg>
-                                <p>somefuture@email.com</p>
+                                <p>info@bap-org.com</p>
                             </div>
                         </Link>
                         <div className="icon-row">
